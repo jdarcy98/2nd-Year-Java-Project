491bb082d302c03a3ad52be02c35f8c082318f0b
jdarcy98 <c16357773@mydit.ie>
2018-04-15 23:44:11 +0100

    Add files via upload

--- FileSearch.java
+++ FileSearch.java
@@ -33,69 +33,43 @@ public class FileSearch
 	public String readLine()
 	{		
 			String line = "no line in file";
-			Scanner sc = null;
+			//String word = "wank";
+			//Scanner sc = null;
+			
 		    try
 		    {
 		    	List<String> list = new ArrayList<>();
-		        sc = new Scanner(new File("posts.txt"));
+		       	Scanner sc = new Scanner(fleExample);
 		        //int count = 0;
-		        if(sc.hasNext())
+		        while(sc.hasNext())
 		        {
-		            /*String sen = sc.nextLine();
-		            for(int i = 0 ; i < sen.length(); i++)
-		            {
-		                if(sen.charAt(i) >= 'A' && sen.charAt(i) <= 'Z')
-		                {
-		                    count++;
-		                    if(count >= 10)
-		                    {
-		                    	System.out.println("This person is shouting");
-		                    }
-		                }
-		            }*/
 		        	list.add(sc.next());
 		        	//System.out.println(list);
-		        	if(list.contains("fuck"))
-		        	{
-		        		System.out.println("Word exists");
-		        	}
 		        	
-		        	else
-		        	{
-		        		System.out.println("Word doesnt exist");
-		        	}
+		        }	
+		        if(list.contains("wanker"))
+		        {
+		        	System.out.println("Word exists");
+		        }
+		        	
+		        else
+		        {
+		        	System.out.println("Word doesnt exist");
 		        }
 
+		        sc.close();
 		    } 
 		    
 		    catch (FileNotFoundException e)
 		    {
 		        System.out.println(e.getMessage());
 		    }
-		    
-		    /*finally
+		   
+		    finally
 		    {
-		        sc.close();
-		    }*/
+		        
+		    }
 		    
-		    /*try
-			{
-		     // I don't need the scanner in any other method, so I can create it here.
-			 Scanner myScanner = new Scanner(fleExample);
-			 
-			  
-			 if (myScanner.hasNextLine())
-			    {
-			      line = myScanner.nextLine();
-			      System.out.println ("I read this line " + line);
-			    }
-
-			myScanner.close();
-			}
-			catch (FileNotFoundException e)
-			{
-				System.out.println(e.getMessage());
-			}*/
 
 		   return line;
 			  
